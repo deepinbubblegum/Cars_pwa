885fc32a7e9c9f6467dce40542cf96f8c2ddc380
Chaiwit Phonkhen <5911110222@mutacth.com>
2020-10-26 22:21:19 +0700

    update sign in css

--- cars_pwa/pages/index.vue
+++ cars_pwa/pages/index.vue
@@ -1,73 +1,86 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        cars_pwa
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+    <img class="user_def_icon" src="~/assets/svg/Asset2.svg" />
+      <div class="sign_in">
+        <p class="text_sign_in">SIGN IN</p>
+
+        <input class="input-box" placeholder="Username" type="text" />
+        <input class="input-box" placeholder="Password" ctype="password" />
+        <button class="input-box sing-btn">SIGN IN</button>
       </div>
-    </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+.user_def_icon {
+  width: 6%;
+  top: 8%;
+  right: 5%;
+  position: absolute;
+  z-index: 1;
+}
+.text_sign_in {
+  left: 50%;
+  font-size: 30px;
+  font-weight: bold;
   text-align: center;
+
+
+}
+.sign_in {
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  display: inline-block;
+  width: 25%;
+  min-width: 280px;
+  display: flex;
+  flex-direction: column;
+  transform: translate(-50%, -50%);
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  border-radius: 10px;
+  padding: 40px;
+}
+.input-box {
+  float: left;
+  margin: 6px;
+  border-radius: 20px;
+  background-color: lightskyblue;
+  border: none;
+  padding: 14px;
+  outline: none;
+  
+}
+.sing-btn {
+  outline: none !important;
+  border: none;
+  background-color: rgba(0, 128, 0, 0.842);
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  
+}
+.sing-btn:hover {
+  background-color: green;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
 }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.card {
+  position: absolute;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+hr.new {
+  border: 1px solid rgba(0, 0, 0, 0.205);
+  width: 100%;
+  border-radius: 1px;
+  margin: 0;
+  padding: 1;
 }
 </style>
